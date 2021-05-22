1af21442fef22484d01f99286a06fdd911c18c18
Bishnu Bhattarai <bishnusyangja@gmail.com>
2021-05-22 19:15:32 +0545

    fixed query with test cases

--- functional_test/test_booking_api.py
+++ functional_test/test_booking_api.py
@@ -6,7 +6,7 @@ from django.test import TestCase
 from model_mommy import mommy
 from rest_framework.test import APIClient
 
-from listings.models import Listing, BookingInfo, BookedListing
+from listings.models import Listing, BookingInfo, BookedListing, HotelRoomType, HotelRoom
 
 
 class BookingAPITestCase(TestCase):
@@ -69,22 +69,25 @@ class BookingAPITestCase(TestCase):
         self.assertEqual(resp.json()['count'], 4)
 
     def test_booking_case_3(self):
-        l1 = mommy.make(Listing)
-        l2 = mommy.make(Listing)
-        l3 = mommy.make(Listing)
-        l4 = mommy.make(Listing)
-        l5 = mommy.make(Listing)
+        l1 = mommy.make(Listing, title="l1")
+        l2 = mommy.make(Listing, title="l2")
+        l3 = mommy.make(Listing, title="l3")
+        l4 = mommy.make(Listing, title="l4")
+        l5 = mommy.make(Listing, title="l5")
+        l6 = mommy.make(Listing, title="l6")
         mommy.make(BookingInfo, listing=l1, price=200)
         mommy.make(BookingInfo, listing=l2, price=300)
         mommy.make(BookingInfo, listing=l3, price=50)
-        mommy.make(BookingInfo, listing=l4, price=50)
-        mommy.make(BookingInfo, listing=l5, price=50)
+        mommy.make(BookingInfo, listing=l4, price=20)
+        mommy.make(BookingInfo, listing=l5, price=10)
+        mommy.make(BookingInfo, listing=l6, price=10)
 
         mommy.make(BookedListing, apartment=l1, reserved_from='2021-01-04', reserved_to='2021-01-08')
         mommy.make(BookedListing, apartment=l2, reserved_from='2021-01-10', reserved_to='2021-01-15')
         mommy.make(BookedListing, apartment=l3, reserved_from='2021-01-15', reserved_to='2021-01-18')
         mommy.make(BookedListing, apartment=l4, reserved_from='2021-01-25', reserved_to='2021-01-29')
         mommy.make(BookedListing, apartment=l5, reserved_from='2021-01-07', reserved_to='2021-01-10')
+        mommy.make(BookedListing, apartment=l6, reserved_from='2021-01-17', reserved_to='2021-01-24')
 
         data = dict(max_price=500,
                     check_in="2021-01-12",
@@ -93,7 +96,55 @@ class BookingAPITestCase(TestCase):
                                **self.get_authentication_headers())
         self.assertEqual(resp.status_code, 200)
 
-        self.assertEqual(resp.json()['count'], 2)
-        self.assertEqual(resp.json()['count'], 2)
+        self.assertEqual(resp.json()['count'], 3)
+
+    def test_booking_case_4(self):
+        l1 = mommy.make(Listing, title="l1")
+        l2 = mommy.make(Listing, title="l2")
+        l3 = mommy.make(Listing, title="l3")
+        l4 = mommy.make(Listing, title="l4")
+        l5 = mommy.make(Listing, title="l5")
+        l6 = mommy.make(Listing, title="l6")
+        mommy.make(BookingInfo, listing=l1, price=200)
+        mommy.make(BookingInfo, listing=l2, price=300)
+        mommy.make(BookingInfo, listing=l3, price=50)
+        mommy.make(BookingInfo, listing=l4, price=20)
+        mommy.make(BookingInfo, listing=l5, price=10)
+        mommy.make(BookingInfo, listing=l6, price=10)
+
+        h1 = mommy.make(HotelRoomType, hotel=l1)
+        h2 = mommy.make(HotelRoomType, hotel=l2)
+        h3 = mommy.make(HotelRoomType, hotel=l3)
+        h4 = mommy.make(HotelRoomType, hotel=l4)
+        h5 = mommy.make(HotelRoomType, hotel=l5)
+        h6 = mommy.make(HotelRoomType, hotel=l6)
+
+        h1r1 = mommy.make(HotelRoom, hotel_room_type=h1, room_number=6)
+        h1r2 = mommy.make(HotelRoom, hotel_room_type=h1, room_number=5)
+        h1r3 = mommy.make(HotelRoom, hotel_room_type=h1, room_number=7)
+
+        h2r1 = mommy.make(HotelRoom, hotel_room_type=h2, room_number=6)
+        h2r2 = mommy.make(HotelRoom, hotel_room_type=h2, room_number=5)
+        h2r3 = mommy.make(HotelRoom, hotel_room_type=h2, room_number=7)
+
+        h3r1 = mommy.make(HotelRoom, hotel_room_type=h3, room_number=6)
+        h3r2 = mommy.make(HotelRoom, hotel_room_type=h3, room_number=5)
+        h3r3 = mommy.make(HotelRoom, hotel_room_type=h3, room_number=7)
+
+        mommy.make(BookedListing, hotel_room=h1r1, reserved_from='2021-01-04', reserved_to='2021-01-08')
+        mommy.make(BookedListing, hotel_room=h2r1, reserved_from='2021-01-10', reserved_to='2021-01-15')
+        mommy.make(BookedListing, hotel_room=h1r2, reserved_from='2021-01-15', reserved_to='2021-01-18')
+        mommy.make(BookedListing, hotel_room=h1r3, reserved_from='2021-01-25', reserved_to='2021-01-29')
+        mommy.make(BookedListing, hotel_room=h2r2, reserved_from='2021-01-07', reserved_to='2021-01-10')
+        mommy.make(BookedListing, hotel_room=h3r1, reserved_from='2021-01-17', reserved_to='2021-01-24')
+
+        data = dict(max_price=500,
+                    check_in="2021-01-12",
+                    check_out="2021-01-20")
+        resp = self.client.get(self.url, data,
+                               **self.get_authentication_headers())
+        self.assertEqual(resp.status_code, 200)
+
+        self.assertEqual(resp.json()['count'], 4)
 
 
--- listings/views.py
+++ listings/views.py
@@ -38,46 +38,24 @@ class ListingAPIView(ReadOnlyModelViewSet):
             data = ser.validated_data
             check_in = data.get('check_in')
             check_in = check_in[0] if isinstance(check_in, list) else check_in
-            check_in = check_in.strftime("%Y-%m-%d")
             check_out = data.get('check_out')
             check_out = check_out[0] if isinstance(check_out, list) else check_out
-            check_out = check_out.strftime("%Y-%m-%d")
             max_price = data.get('max_price')
             max_price = max_price[0] if isinstance(max_price, list) else max_price
             if check_in and check_out:
-                qs = qs.filter(
-                        # Q(booked_apartment__isnull=True) |
-                        # Q(booked_apartment__reserved_to__lte=check_in) |
-                        # Q(booked_apartment__reserved_from__gte=check_out) |
-                        ~Q(booked_apartment__reserved_from__gte=check_in, booked_apartment__reserved_to__lte=check_in)).filter(
-                        ~Q(booked_apartment__reserved_from__gte=check_out, booked_apartment__reserved_to__lte=check_out)
-                        #
-                        # ~Q(booked_apartment__reserved_from__lte=check_in, booked_apartment__reserved_from__gte=check_out) |
-                        # ~Q(booked_apartment__reserved_to__lte=check_in, booked_apartment__reserved_to__gte=check_in) |
-                        # ~Q(booked_apartment__reserved_to__lte=check_out, booked_apartment__reserved_to__gte=check_out) |
-                        # ~Q(booked_apartment__reserved_from__lte=check_in, booked_apartment__reserved_from__gte=check_in) |
-                        # ~Q(booked_apartment__reserved_from__lte=check_out, booked_apartment__reserved_from__gte=check_out) |
+                qs = qs.exclude(
+                        Q(booked_apartment__reserved_from__lte=check_in, booked_apartment__reserved_to__gte=check_in) |
+                        Q(booked_apartment__reserved_from__lte=check_out, booked_apartment__reserved_to__gte=check_out) |
+                        Q(booked_apartment__reserved_from__gte=check_in, booked_apartment__reserved_to__lte=check_out) |
 
-                        # Q(hotel_room_types__hotel_rooms__booked_hotel_room__isnull=True) |
-                        # Q(hotel_room_types__hotel_rooms__booked_hotel_room__reserved_to__lte=check_in) |
-                        # Q(hotel_room_types__hotel_rooms__booked_hotel_room__reserved_from__gte=check_out) |
-                        # ~Q(hotel_room_types__hotel_rooms__booked_hotel_room__reserved_from__gte=check_in,
-                        #    hotel_room_types__hotel_rooms__booked_hotel_room__reserved_to__lte=check_in)
-                        # ~Q(hotel_room_types__hotel_rooms__booked_hotel_room__reserved_from__gte=check_out,
-                        #    hotel_room_types__hotel_rooms__booked_hotel_room__reserved_to__lte=check_out)
-                # ).exclude(
-                #
-                #     Q(booked_apartment__reserved_from__gte=check_in, booked_apartment__reserved_to__lte=check_in) |
-                #     Q(booked_apartment__reserved_from__gte=check_out, booked_apartment__reserved_to__lte=check_out) |
-                #
-                #     Q(booked_apartment__reserved_from__lte=check_in, booked_apartment__reserved_from__gte=check_out) |
-                #     Q(booked_apartment__reserved_to__lte=check_in, booked_apartment__reserved_to__gte=check_in) |
-                #     Q(booked_apartment__reserved_to__lte=check_out, booked_apartment__reserved_to__gte=check_out) |
-                #     Q(booked_apartment__reserved_from__lte=check_in, booked_apartment__reserved_from__gte=check_in) |
-                #     Q(booked_apartment__reserved_from__lte=check_out, booked_apartment__reserved_from__gte=check_out)
-                )
+                        Q(hotel_room_types__hotel_rooms__booked_hotel_room__reserved_from__lte=check_in,
+                          hotel_room_types__hotel_rooms__booked_hotel_room__reserved_to__gte=check_in) |
+                        Q(hotel_room_types__hotel_rooms__booked_hotel_room__reserved_from__lte=check_out,
+                          hotel_room_types__hotel_rooms__booked_hotel_room__reserved_to__gte=check_out) |
+                        Q(hotel_room_types__hotel_rooms__booked_hotel_room__reserved_from__gte=check_in,
+                          hotel_room_types__hotel_rooms__booked_hotel_room__reserved_to__lte=check_out))
+
             if max_price:
                 qs = qs.filter(booking_info__price__lt=max_price)
-            # print(qs.query)
         return qs
 
